82d237d37a9d361203ebf4a12e4c27e5913fd010
unknown <you@example.com>
2020-02-04 09:58:10 +0100

    w3

--- warmUp3/warmUp3.js
+++ warmUp3/warmUp3.js
@@ -3,18 +3,50 @@
 // - returns whichever number is the greater (higher) number.
 // ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
 
+function greaterNum(a,b){
+    if(a<b){
+        return 'The greater number of'+' '+ a +' '+'and'+ b+' '+'is'+' 'b;
+    }
+return 'The greater number of'+' '+ a +' '+'and'+ b+' '+'is'+' 'b;
+}
 
 
 // 2) Write a function named isEven using  for loop that 
 // -  iterate from x to y.
 // -  return array contain the even values,
 //  ex:  isEven(1,10) => [2,4,6,8,10]
+var array=[x,y];
+while(x<=y){
+    array =[x,x++];
+}
+function isEven(array){
+    for(var i=0; i<array.length ; i++){ 
+        if(array[i] %2 !== 0){ 
+             array.splice(i,1); 
+             i--; 
+        } 
+   } 
+   return array; 
+}
+
 
 
 //3) write a function named sum that 
 // - Use a while loop to add up the numbers from x to y.
 // ex sum(1,5) => 15
-
+var n=[x,y];
+while(x<=y){
+    n =[x,x++];
+}
+function sum(n) { 
+	var counter = 1; 
+	var total = 0; 
+	while (counter <= n) { 
+	total = total + counter; 
+	counter = counter + 1; 
+	} 
+	return total; 
+	}
 
 //4) Write a function named factorial that 
 // -  Use Recursion to calculate the factorial of a number
