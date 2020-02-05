67e3ae6f0cd46524cc7c9e908edffbd55a9419b0
kyri777 <lachtarkhaireddine@gmail.com>
2020-02-05 16:03:38 +0100

    w3d3

--- warmUp4/warmUp4.js
+++ warmUp4/warmUp4.js
@@ -7,3 +7,33 @@
 	}
 // 3-Write a JavaScript function that accepts a number as a parameter and check if the number is prime or not.
 // 4-write a function that accepts two numbers as parameters and returns true if the two parameters have the same value , false if not
+function myTrueFun(){
+	return true
+}
+
+function typeFunc(value){
+	if ((value)==="str")
+	{
+	   console.log(true)
+    }
+	else return (false)
+	
+}
+
+function prime(num){
+	var result=0
+	if ( num%2===1){
+	    result=num/(3||7||11||5)
+}
+	 if (result%2===0){
+	      console.log("number is not prime")
+	}
+	else 
+	return " number is prime" 
+}
+function value(num1,num2){
+	if(num1===num2){
+		console.log (true)
+	}else
+	return false 
+}
