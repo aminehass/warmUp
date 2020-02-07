7c80dcc8e5bb39ffe470e3ed84a40449e3699892
yosri00 <max.power1212222@gmail.com>
2020-02-07 10:01:01 +0100

    ahmed

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -2,19 +2,28 @@
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
 
 function objKey(obj) {
-  // your code here...
+  var keyArray=[];
+  return keyArray=Object.keys(obj);
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+  var keyArray=Object.keys(obj);
+  return keyArray.length; 
 }
 
 // 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
 // objSort([{id:2,value:50},{id:0,value:70},{id:1,value:40}]) ==> [{id:0,value:70},{id:1,value:40},{id:2,value:50}]
 
-function objSort(obj) {
-  // your code here...
+function objSort(arrayObj) {
+  var sortedArray=[];
+
+  for(var i=arrayObj.length;i<0;i--) {
+    if (arrayObj[i].id===i) {
+      sortedArray.push(arrayObj[i]);
+    }
+  }
+  return sortedArray;
 }
