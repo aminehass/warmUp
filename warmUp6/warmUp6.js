d8046b14a1a4fda6beb9a37f529ca6dd0fff7386
unknown <you@example.com>
2020-02-07 14:38:51 +0100

    amine

--- warmUp6/warmUp6.js
+++ warmUp6/warmUp6.js
@@ -1,15 +1,23 @@
 // 1 - Write a function named objKey that take an object as a parameter and returns an array containing the keys of the object
 // objKey({name:'ahmed',age:20}) ==> ['name','age']
 
+
+
 function objKey(obj) {
-  // your code here...
+  var array = [];
+      array.push(Object.keys(obj));      
+  return array;
 }
 
 // 2 - Write a function named objLength to get the length of a JavaScript object
 // objLength({name:'ahmed',age:20}) ==> 2
 
 function objLength(obj) {
-  // your code here...
+  var x = 0;
+  for(var key in obj) { 
+    x = x + 1;
+  }
+  return x;
 }
 
 // 3 - Write a function named objSort to sort an array of JavaScript objects based on the id
