63df518bed492da1f0a37cf01fb5698957263f5b
alisnakesou <lkollasfbt@gmail.com>
2020-02-08 11:18:39 +0100

    Sorry for being late

--- warmUp7/warmUp7.js
+++ warmUp7/warmUp7.js
@@ -1,8 +1,36 @@
 //1. Write a JavaScript function to check whether an `input` is an array or not
 // ex isArray([1,2])=>true
 //    isArray(5)=>false
+
+function isAarray(collection){
+    if (Array.isArray(collection)){
+        return true;
+    }
+    return false;
+}
 //2-Write a JavaScript function to get the first n element of an array.
 //ex  first([1,2,3],1)=>[1]
 //    first([1,2,3,4]2)=>[1,2]
+
+function firstElements(array, n){
+    var acc= array.filter(function(element,i){
+        while(i<n){
+            return element;
+        }
+    });
+    return acc;
+}
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+
+function convertObject(obj){
+    var acc=[];
+    var aux=[];
+    for(var key in obj){
+        acc.push(key, obj[key]);  
+        aux.push(acc);
+        acc=[];
+          
+    }
+    return aux;
+}
