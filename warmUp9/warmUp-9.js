1bf3aa2601ca103021389576886c39ba6e52bdbe
alisnakesou <lkollasfbt@gmail.com>
2020-02-12 09:23:45 +0100

    i finished

--- warmUp9/warmUp-9.js
+++ warmUp9/warmUp-9.js
@@ -2,7 +2,26 @@
 // -name of females in your class
 // -name of males in your class
 // -name of your class instructors 
+var females=['siwar', 'nouha', 'halima'];
+var males=['amine', 'afif', 'firass', 'ali', 'belgacem', 'houssem', 'taha'];
+var instructors=['walid', 'cassels', 'matt', 'seif', 'hadhemy', 'safa'];
+
 
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
-
+function middle(array){
+    if(array.length%2===0){
+        return array[(array.length/2)-1]+' & '+array[array.length/2]
+    }
+    return array[(array.length-1)/2];
+}
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+function evenIndexes(array){
+    var arr=[];
+    for(var i=0; i<array.length; i++){
+        if (i%2===0){
+            arr.push(array[i]*2);
+        }
+    }
+    return arr;
+}
