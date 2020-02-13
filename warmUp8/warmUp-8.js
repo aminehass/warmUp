63242524636c4aa22cbc7c8b44d3f7fae4af6b26
kyri777 <lachtarkhaireddine@gmail.com>
2020-02-13 11:38:10 +0100

    warmUp

--- warmUp8/warmUp-8.js
+++ warmUp8/warmUp-8.js
@@ -1,3 +1,23 @@
 // 1- Using recursion Write a JavaScript program to find the greatest common divisor (gcd) of two positive numbers.
 // 2-Write a function called sum that accepts two numbers as parameters, and sum them together but without suming them togther directly
 //you can only add one at each summetion, you'll need to use recursion in this.
+
+
+function add(num1,num2){
+    var sum=num1;
+       for (num2>0, num1+num2===sum++){
+        sum++ ;
+  }; 
+   return sum ;
+  } 
+  
+  function gcd(x, y) {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
--- warmUp9/warmUp-9.js
+++ warmUp9/warmUp-9.js
@@ -6,3 +6,28 @@
 // 2-write a function that takes an array as an argument and returns the element that is located in the middle of that array.
 
 // 3-Change all the numbers in the array to be multiplied by two for even indexes.
+
+
+var females=['Nouha', 'Siwar']
+var males=["Amin", "Firas", "Aymen", "Housem" ]
+var instructors=['Wamid', 'Matt', 'Cassels', 'seif']
+
+function midd (array){
+  if (array.length%2===0){
+    return "invalid input"
+   }
+
+     return array[(array.length-1)/2]
+ }
+
+
+
+ function evenby2(array) {
+ var acc=array
+for (var i = 0; i < array.length; i++) {
+    acc.push(array[2 * i + 1]*2);
+  }
+
+  return acc 
+}
+
