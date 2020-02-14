2a5524190996656c18eb8405c5823dde62c6d393
kyri777 <lachtarkhaireddine@gmail.com>
2020-02-14 12:02:38 +0100

    warm7&10up

--- warmUp7/warmUp7.js
+++ warmUp7/warmUp7.js
@@ -6,3 +6,33 @@
 //    first([1,2,3,4]2)=>[1,2]
 //3- Write a JavaScript function to convert an object into a list of `[key, value]` pairs
 //ex: convert({name:'ahmed',age:20})=>[['name','ahmed'],['age', 20]]
+
+//Q1:This function returns a boolean
+function checkForArray (input) {
+    if ( Array.isArray (input)){
+        return true;-
+    };
+    else {
+        return false;
+    };
+}
+//Q2
+function firstElementsOfArray (array, num){
+    return array.slice(0,num);
+}
+//Q3: we need an each function first
+function each (object , fn){
+    if ((!Array.isArray (object))&&(typeof object === "object")){
+        for (var key in object){
+            fn (object[key], key)
+        };
+    };
+}
+
+function convert (object){ 
+    var array=[];
+    each (object, function (element, key){
+        array.push([key, element])
+    });
+    return array
+}
