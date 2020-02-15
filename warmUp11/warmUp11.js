afb39c428dd11f39374b90dde87f717e0c419ced
AbobkerElaghel <abobkerelaghel@gmail.com>
2020-02-15 10:05:47 +0100

    finish

--- warmUp11/warmUp11.js
+++ warmUp11/warmUp11.js
@@ -9,3 +9,42 @@
     • Write a function searchMates that, given a query and an array of Mates,
       searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 */
+function makeClassmate(name, age, gender) {
+  return {
+    name:name,
+    age:age,
+    gender:gender
+  };
+}
+var classMate1=makeClassmate('Ali', 30, 'male');
+var classMate2=makeClassmate('Siwar', 26, 'female');
+var classMate3=makeClassmate('Taha', 41, 'male');
+
+var classMates=[classMate1,classMate2,classMate3];
+
+function displayFriend(classMate) {
+  return classMate.name+' is '+classMate.age+', gender '+classMate.gender+'.';
+}
+function addFriend(classMates, classMate) {
+  return classMates.push(classMate);
+}
+
+function nbOfMale(classMates) {
+  var c=0;
+  for(var i=0; i<classMates.length; i++) {
+    if (classMates[i].gender === 'male') {
+          c=c+1;
+    }
+  }
+  return c;
+}
+
+function searchMates(n, classMates) {
+  var display='';
+  for(var i=0; i<classMates.length; i++) {
+    if(classMates[i].age === n) {
+      display=display+ i +' .'+displayFriend(classMates[i])+ '\n';
+    }
+  }
+  return display;
+}
