6ed958c104c88131fca9114d6e0e1d10166deb57
alisnakesou <lkollasfbt@gmail.com>
2020-02-15 10:08:27 +0100

    i didn't finish the last function

--- warmUp11/warmUp11.js
+++ warmUp11/warmUp11.js
@@ -9,3 +9,56 @@
     • Write a function searchMates that, given a query and an array of Mates,
       searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 */
+
+function makeClassmates (name, age, glasses, sex) {
+  return {
+    name: name,
+    age: age,
+    glasses: glasses,
+    sex: sex
+  }
+}
+
+var classmate1 = makeClassmates ("Amine", 41, "no", "male");
+var classmate2 = makeClassmates ("Afif", 27, "no", "male");
+var classmate3 = makeClassmates ("Ali", 29, "no", "male");
+var classmate4 = makeClassmates ("Taha", 35, "yes", "male");
+var classmate5 = makeClassmates ("Houssem", 31, "yes", "male");
+var classmate6 = makeClassmates ("Amine Ziadi", 28, "yes", "male");
+var classmate7 = makeClassmates ("Nouha", 28, "yes", "female");
+var classmate8 = makeClassmates ("Siwar", 28, "no", "female");
+
+var classmates = [classmate1, classmate2, classmate3, classmate4, classmate5, classmate6, classmate7];
+
+
+function displayFriend (mate) {
+  for (var key in mate) {
+    return "My classmate " + mate.name + " has " + mate.age + " years old." 
+      }
+}
+
+function addFriend (mate) {
+  classmates.push(mate);
+}
+
+function nbOfMale (array) {
+  var nbofmale = 0;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].sex === "male") {
+      nbofmale += 1;
+    } else {
+      nbofmale +=0;
+    }
+  }
+  return nbofmale;
+}
+
+function searchMates (query, array) {
+  for(var i = 0; i < array.length; i++) {
+    if ( (/query/g) in array[i]  ){
+      return displayFriend (array[i]);
+    }
+  }
+}
+
+
