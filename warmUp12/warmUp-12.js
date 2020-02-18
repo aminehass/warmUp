d008a2eac1195542b43b201ec35a206590517c18
alisnakesou <lkollasfbt@gmail.com>
2020-02-18 09:22:15 +0100

    i finished

--- warmUp12/warmUp-12.js
+++ warmUp12/warmUp-12.js
@@ -2,11 +2,30 @@
  //starting from 0, e.g.:
  mult(3); // => 6
  mult(4); // => 24
+
+
  function mult(n) {
  // your code is here
+    var acc = 1;
+    for (var i = 1; i <= n; i++)
+    {
+        acc *= i;
+    }
+    return acc;
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+
+
+
+function buildSring (n) {
+    var str = '1';
+    var i = 1;
+    while (i++ < n){
+        str += i + '' + i;
+    }
+    return str;
+}
