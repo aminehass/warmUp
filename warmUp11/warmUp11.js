ff0d3b9221e972be43c24f5a69281d9ea35d39e6
NouhaChorfi <nouhachorfi2@gmail.com>
2020-02-18 09:12:48 +0100

    warmup11

--- warmUp11/warmUp11.js
+++ warmUp11/warmUp11.js
@@ -9,3 +9,77 @@
     • Write a function searchMates that, given a query and an array of Mates,
       searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
 */
+
+function  factoryClassMates(name,age, gendre, nationality){
+  return {
+          name: name ,
+          age:age,
+          gendre: gendre,
+          nationality: nationality          
+  }
+}
+
+var classmates=[];
+
+
+function each(coll, func) { 
+  if (Array.isArray(coll)) { 
+        for (var i = 0; i < coll.length; i++) { 
+              func(coll[i], i); 
+        } 
+  } else { 
+        for (var key in coll) { 
+              func(coll[key], key); 
+        } 
+  } 
+}
+
+function displayFriend (arrayOfClassmates,name){
+  var friend="";
+  each (arrayOfClassmates, function (element, i){
+    if(element.name===name){
+      if (element.gendre==="male"){
+          friend= element.name+ " is "+ element.age +" years old, he is " + element.nationality + ".\n"
+      }
+      else if (element.gendre==="female"){
+        friend= element.name+ " is "+ element.age +" years old, she is " + element.nationality + ".\n"
+      }
+    }
+  })
+  return friend;
+}
+
+function addFriend(arrayOfClassmates,name, age , gendre , nationality){
+  return arrayOfClassmates.push(factoryClassMates(name , age, gendre, nationality));
+}
+
+addFriend(classmates,"Siwar", 30, "female", "Tunisian");
+addFriend(classmates,"Amine", 40, "male", "Tunisian");
+addFriend(classmates,"Omar", 19, "female", "Tunisian");
+addFriend(classmates,"ayman", 37, "female", "Tunisian");
+addFriend(classmates,"Abdulhadi", 30, "female", "Lybian");
+
+function nbOfMale(arrayOfClassmates){
+  var counter = 0;
+  each (arrayOfClassmates, function (element, i){
+      if (element.gendre==="male"){
+        counter=counter+1
+      }
+  })
+  return counter
+}
+
+// Write a function searchMates that, given a query and an array of Mates,
+//searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
+
+function searchMates(query, arrayOfClassmates){
+  var matchingMate=""
+  each (arrayOfClassmates, function(element,i){
+      each(element, function(value, key){
+        if(value===query){
+            matchingMate=displayFriend (arrayOfClassmates,element.name);
+        }
+      })
+  })
+  return matchingMate;
+}
