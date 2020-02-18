9740e82af58b36213f8e842b9f721c9a3a2142dd
NouhaChorfi <nouhachorfi2@gmail.com>
2020-02-18 09:40:11 +0100

    warmUp12

--- warmUp12/warmUp-12.js
+++ warmUp12/warmUp-12.js
@@ -2,11 +2,38 @@
  //starting from 0, e.g.:
  mult(3); // => 6
  mult(4); // => 24
- function mult(n) {
- // your code is here
+
+ function mult ( n ) {
+     var multiplied = 0;
+    for ( var i=0 ; i<=n ; i++ ) {
+        if ( i === 0){
+            multiplied = 1;
+        }
+        else{
+            multiplied = multiplied * i;
+        }
+    }
+    return  multiplied
 }
 
 // 2- Use a while loop to build a single string with the numbers 1 through n
 // separated by the number next to the current number. 
 //Have it return the new string.
 // eg= 1 2 2 3 3 4 4 5 5 6 6 ...
+
+function numberToString ( n ){
+
+    var result = "1";
+    var counter = 1;
+
+    while ( counter<=n ) {
+        if (n===1){
+            return result="1"
+        }
+        else {
+            result = result + " " + (counter + 1) + " " + (counter + 1);
+            counter++;
+        }
+    }
+    return result 
+}
