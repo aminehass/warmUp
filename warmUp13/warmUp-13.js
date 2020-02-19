83c259968a24d574cba2cb02c95301e3e2de69db
aminehass <60433383+aminehass@users.noreply.github.com>
2020-02-19 10:31:41 +0100

    warmUp13

--- warmUp13/warmUp-13.js
+++ warmUp13/warmUp-13.js
@@ -2,3 +2,17 @@
 //For example:  array: [ 1, 2, 3, 4 ], sub:[ 2, 3, 4 ] => true
 //              array: [ 2, 2, 2, 3, 4 ], sub:[ 2, 4, 3 ] => true
 //              array: [ 2, 3, 3, "a" ],  sub:[ 3, 3, 3, "a" ] => false
+function isSubset(array, sub) {
+    var result = [];
+    for(var i = 0; i < sub.length; i++) {
+        for(var j =0; j < array.length; j++) {
+            if( sub[i] === array[j]) {
+                result.push(array[j]);
+            }
+        }
+    }
+    if(sub === result) {
+        return true;
+    }
+    return false;
+}
